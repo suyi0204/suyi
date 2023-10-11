d83db46ab89d7ff3bee4c4c9165210886b0695de
suyi0204 <11056046@ntub.edu.tw>
2023-10-11 16:15:46 +0800

    1011

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from mysite.models import POST
+from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
+
+def homepage(request):
+    posts = POST.objects.all()
+    now = datetime.now()
+    return render (request, 'index.html',locals())
+
+def showpost(request, slug):
+    POST = POST.objects.get(slug=slug)
+    #select * from post where slug=%slug
+    return render(request,'post.html',locals())
+
+
+    
+'''
+def homepage(request):
+    posts = POST.objects.all() #select * from post
+    post_list = list( )
+    for counter,post in enumerate(posts):
+        post_list.append(f'No. {counter}-{post} <br>')
+    return HttpResponse(post_list)
+'''
--- suyi/urls.py
+++ suyi/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from mysite import views as mv
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',mv.homepage,name="homepage"),
+    path('POST/<slug:slug>/',mv.showpost,name="showpost")
 ]
